592839cfb82d81c4c024d4327e09e4af12e949cf
Aylin Bekem <bekemaylin@gmail.com>
2022-03-15 20:17:38 -0400

    thymeleaf

---
+++ Spring-09-Spring-TymeLeaf/src/main/java/com/cydeo/Spring09SpringTymeLeafApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09SpringTymeLeafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09SpringTymeLeafApplication.class, args);
+    }
+
+}
